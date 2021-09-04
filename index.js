f7f349ea5eb80ea3d93d2c3dcce8a5d0aecaab47
N7hoatt <tranthaihoa1412@gmail.com>
2021-09-04 16:50:22 +0700

    add

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require("express")
+const app = express()
+
+const http = require("http")
+const sever = http.createServer(app)
+const { Server} = require("socket.io")
+
+const io = new Server(sever)
+
+app.get('/', (req, res)  =>{
+    res.sendFile(__dirname + "/index.html");
+})
+
+io.on("connection",  (socket) =>{
+    socket.on('chat-sent', data =>{
+        io.emit('user-chat', data)
+    })
+})
+
+sever.listen(4000, ()  =>{
+    console.log("sever listen 4000")
+})

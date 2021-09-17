cfbb89a1bb913c8965cccc467885e7c3bc357807
N7hoatt <tranthaihoa1412@gmail.com>
2021-09-17 21:00:26 +0700

    add saved chat

--- index.js
+++ index.js
@@ -1,23 +1,71 @@
 const express = require("express");
 const app = express();
-
 const http = require("http");
 const sever = http.createServer(app);
 const { Server } = require("socket.io");
-
 const io = new Server(sever);
+const mongodb = require("mongodb").MongoClient;
 
+// connect to localhost
+sever.listen(4000);
+
+// send HTML page
 app.use(express.static(__dirname));
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
 
-io.on("connection", (socket) => {
-  socket.on("chat-sent", (data) => {
-    io.emit("user-chat", data);
+// connect to MongoDB
+mongodb.connect('mongodb://127.0.0.1/TUTORIAL', (err, client) =>{
+  if(err){
+    throw err;
+  }
+  console.log('mongodb connected');
+  io.on('connection', (socket) =>{
+    const chat = client.db('client');
+    
+    // create send status function
+    sendStatus = (s) =>{
+      socket.emit('status', s);
+    }
+
+    // get chat from mongo collection
+    chat.collection('chat').find().sort({_id:1}).toArray((err, res)=>{
+      if(err){
+        throw err;
+      }
+
+      // emit the result
+      socket.emit('output', res);
+    });
+
+    // handle output
+    socket.on('input', (data)=>{
+      const name = data.name;
+      const message = data.message;
+      
+      if(name == '' || message == ''){
+        sendStatus('please enter and chat and message');
+      } else{
+        chat.collection('chat').insertOne({name: name, message: message}, ()=>{
+          io.emit('output', [data]);
+
+          // Send clear status
+          sendStatus({
+            message: 'message sent',
+            clear: true
+          });
+        });
+      }
+    });
+
+    //handle clear
+    socket.on('clear', ()=>{
+      //remove chat collection
+      chat.collection('chat').remove({}, ()=>{
+        socket.emit('cleared');
+      });
+    });
   });
 });
 
-sever.listen(4000, () => {
-  console.log("sever listen 4000");
-});
--- send-chat.js
+++ send-chat.js
@@ -1,29 +1,65 @@
-const name = prompt("What your name?");
-const socket = io();
-const chatForm = document.querySelector("#chat-form");
-const chatMes = document.querySelector("#chat-message");
+// get element
+const status = document.getElementById('status');
+const username = document.getElementById('username');
+const texting = document.getElementById('texting');
+const messages = document.getElementById('messages');
+const clearBtn = document.getElementById('clearBtn');
 
-chatForm.addEventListener("submit", (e) => {
-  e.preventDefault();
-  const message = chatMes.value;
-  socket.emit("chat-sent", {
-    name,
-    message,
+//set status default
+const statusDefault = status.textContent;
+const setStatus = (s)=>{
+  // set status
+  status.textContent = s;
+  if(s != statusDefault){
+    setTimeout(()=>{
+      setStatus(statusDefault);
+    }, 4000);
+  }
+}
+
+// connect to socket.io
+const socket = io.connect();
+if(socket != undefined){
+  console.log("connected to socket.io...");
+  socket.on('output', (data)=>{
+    if(data.length){
+      for(var x = 0; x < data.length; ++x){
+        const message = document.createElement('div');
+        message.textContent = data[x].name + ": " + data[x].message;
+        message.setAttribute('id', "message-" + x);
+        message.setAttribute('class', "message");
+        messages.appendChild(message);
+      }
+    }
   });
-  chatMes.value = "";
-});
 
-const messages = document.querySelector("#messages");
-socket.on("user-chat", (message) => {
-  const chatName = document.createElement("span");
-  const chatMessage = document.createElement("span");
-  chatName.setAttribute("class", "Username");
-  chatMessage.setAttribute("class", "message");
-  chatName.textContent = message.name;
-  chatMessage.textContent = message.message;
-  const hr = document.createElement("hr");
-  messages.appendChild(chatName);
-  messages.append(" : ");
-  messages.appendChild(chatMessage);
-  messages.append(hr);
-});
+  //handle status
+  socket.on('status', (data)=>{
+    setStatus((typeof data === 'object') ? data.message : data);
+
+    //check status is clear
+    if(data.clear){
+      texting.value = '';
+    }
+  });
+
+  // handle input
+  texting.addEventListener('keydown', (e)=>{
+    if(e.keyCode === 13 && e.shiftKey == false){
+      socket.emit('input', {
+        name:username.value,
+        message:texting.value
+      });
+    }
+  });
+
+  // handle clear
+  clearBtn.addEventListener('click', ()=>{
+    console.log("clear");
+    socket.emit('clear');
+  });
+
+  socket.on('cleared', ()=>{
+    messages.textContent = '';
+  });
+}
